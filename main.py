4b4d3702eb95f0aa3c69cd7c03302ce82beccd10
Sidcrosby2-0 <hatom@bk.ru>
2024-01-16 10:33:02 +0700

    Change view. Add button ~Open folder~. Gange language RUS -> ENG

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os.path
 from tkinter import ttk
 import tkinter as tk
 from tkinter import messagebox
@@ -12,41 +13,49 @@ def close_window():
     window.destroy()
 
 
+def open_folder():
+    path1 = os.path.realpath(path)
+    os.startfile(path1)
+
+
 def download():
     try:
         link_1 = video_link.get()
         u2be_link = pytube.YouTube(link_1)
         video = u2be_link.streams.get_highest_resolution()
-        video.download(output_path='C:/U2BE Downloader/')
-        messagebox.showinfo(None, "Video downloaded in  C:/U2BE Downloader")
+        video.download(output_path=path)
+        messagebox.showinfo(None, "Video downloaded")
     except:
-        messagebox.showinfo('ERROR', 'URL is not definded')
+        messagebox.showinfo('ERROR', 'URL is not defined')
 
 
 if __name__ == '__main__':
+    path = 'C:/U2BE Downloader/'
 
     window = tk.Tk()
-    window.geometry('450x250')
+    window.geometry('400x200')
     window.resizable(False, False)
     window.title('U2BE DOWNLOADER')
     window.config(bg='#ed9b9b')
+    window.iconbitmap('u2be_l.ico')
 
-    lb1 = tk.Label(window, text='YOUTUBE VIDEO DOWNLOADER', font='Arial,15,bold',
-                   bg='#ed9b9b')
+    lb1 = tk.Label(window, text='YOUTUBE VIDEO DOWNLOADER', font=('Times', '15', 'bold'), fg='#ec1316', bg='#ed9b9b')
     lb1.pack(padx=5, pady=10)
 
-    lb2 = tk.Label(window, text='Link video', font='Arial,15,bold', bg='#ed9b9b')
-    lb2.place(x=10, y=100)
+    lb2 = tk.Label(window, text='Link video', font=('Times', '15', 'bold'), fg='#ec1316', bg='#ed9b9b')
+    lb2.place(x=10, y=80)
 
     video_link = tk.StringVar()
-    link_name = tk.Entry(window, textvariable=video_link, font='Arial,15,bold')
-    link_name.place(x=150, y=100)
+    link_name = tk.Entry(window, textvariable=video_link, font=('Arial', '10'))
+    link_name.place(x=120, y=85)
 
-    btn1 = tk.Button(window, text='Download', font='Impact,8,bold', bd=2, command=download)
-    btn1.place(x=360, y=100)
-    btn2 = tk.Button(window, text='Clear', font='Arial,8,bold', bd=2, command=clear)
-    btn2.place(x=140, y=160)
-    btn3 = tk.Button(window, text='Exit', font='Arial,8,bold', bd=2, command=close_window)
-    btn3.place(x=240, y=160)
+    btn1 = tk.Button(window, text='Download', font=('Arial', '10', 'bold'), fg='#ec1316', bd=2, command=download)
+    btn1.place(x=280, y=80)
+    btn2 = tk.Button(window, text=' Clear ', font=('Arial', '10', 'bold'), bd=2, command=clear)
+    btn2.place(x=80, y=150)
+    btn4 = tk.Button(window, text='Open folder', font=('Arial', '10', 'bold'), bd=2, command=open_folder)
+    btn4.place(x=165, y=150)
+    btn3 = tk.Button(window, text='  Exit  ', font=('Arial', '10', 'bold'), bd=2, command=close_window)
+    btn3.place(x=280, y=150)
 
     window.mainloop()
